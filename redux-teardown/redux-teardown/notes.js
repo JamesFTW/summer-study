c80a98b9ff39b276c648c865de472bf37f30b98d
James Andrews <bdaddyja@gmail.com>
2018-06-08 13:11:06 -0700

    Added notes.js

---
+++ redux-teardown/redux-teardown/notes.js
@@ -0,0 +1,16 @@
+/***************************
+Writing something like this
+
+const newnote = {
+  id,
+  content: ''
+}
+
+is the same as writing this
+
+const newnote = {
+  id: id,
+  content: ''
+}
+****************************/
+
