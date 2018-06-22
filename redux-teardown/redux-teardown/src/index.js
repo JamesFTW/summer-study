e52201f043faaebe4bc8f698ce41ace6a86f623e
James Andrews <bdaddyja@gmail.com>
2018-06-21 21:26:26 -0700

    NoteList component maps NoteLinks to an <ul>

--- redux-teardown/redux-teardown/src/index.js
+++ redux-teardown/redux-teardown/src/index.js
@@ -148,6 +148,20 @@ const NoteLink = ({note, onOpenNote}) => {
   </li>
 }
 
+const NoteList = ({notes, onOpenNote}) => {
+  <ul className="note-list">
+    {
+      Object.keys(notes).map(id =>
+        <NoteLink
+          key={id}
+          note={notes[id]}
+          onOpenNote={onOpenNote}
+        />
+      )
+    }
+  </ul>
+}
+
 const NoteApp = ({notes}) => (
   <div>
     <ul className="note-list">
